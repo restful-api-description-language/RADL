eebdb15203de02ac3e4e2191b9b0521990eb7add
Remon Sinnema <remon.sinnema@emc.com>
2015-05-19 11:28:23 +0200

    Fixed syntax error

--- deploy_to_bintray.sh
+++ deploy_to_bintray.sh
@@ -1,7 +1,7 @@
-  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "TRAVIS_SECURE_ENV_VARS" == "true"]; then
-    for package in "core" "gradle"; do
-      for path in java/$package/build/libs/*; do
-        echo curl -T $path -u$USER:key https://api.bintray.com/content/radl/RADL/$(basename $path)\;bt_package=radl-$package\;bt_version=$TRAVIS_TAG\;publish=1\;override=1
-      done
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  for package in "core" "gradle"; do
+    for path in java/$package/build/libs/*; do
+      echo curl -T $path -u$USER:key https://api.bintray.com/content/radl/RADL/$(basename $path)\;bt_package=radl-$package\;bt_version=$TRAVIS_TAG\;publish=1\;override=1
     done
-  fi
+  done
+fi
