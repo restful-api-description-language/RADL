3066b9418c6bd35a4c01104a3c3347da1672d956
Remon Sinnema <remon.sinnema@emc.com>
2015-05-19 12:38:34 +0200

    Reverted Bintray path

--- deploy_to_bintray.sh
+++ deploy_to_bintray.sh
@@ -2,7 +2,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && 
   for package in "core" "gradle"; do
     for path in java/$package/build/libs/*; do
       version=$(basename $path | awk -F- '{print $3}' | cut -d. -f1-3)
-      curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/radl/radl-$package/$version/$(basename $path)\;bt_package=radl-$package\;bt_version=$version\;publish=1\;override=1
+      curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/$(basename $path)\;bt_package=radl-$package\;bt_version=$version\;publish=1\;override=1
     done
   done
 fi
