0acba75f36d5189b1dd59c2aced500557edbd051
Remon Sinnema <remon.sinnema@emc.com>
2015-05-19 12:21:54 +0200

    Fixed paths in Bintray

--- deploy_to_bintray.sh
+++ deploy_to_bintray.sh
@@ -1,7 +1,8 @@
-if [ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   for package in "core" "gradle"; do
     for path in java/$package/build/libs/*; do
-      curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/$(basename $path)\;bt_package=radl-$package\;bt_version=$(basename $path | awk -F- '{print $3}' | cut -d. -f1-3)\;publish=1\;override=1
+      version=$(basename $path | awk -F- '{print $3}' | cut -d. -f1-3)
+      curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/radl/radl-$package/$version/$(basename $path)\;bt_package=radl-$package\;bt_version=$version\;publish=1\;override=1
     done
   done
 fi
