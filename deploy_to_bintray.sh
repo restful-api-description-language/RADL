7787af12c8ba51eb6ebcc735bd7df10db08659e7
Remon Sinnema <remon.sinnema@emc.com>
2015-05-19 13:20:17 +0200

    Only deploy to Bintray when pushing a tag

--- deploy_to_bintray.sh
+++ deploy_to_bintray.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   for package in "core" "gradle"; do
     for path in java/$package/build/libs/*; do
       version=$(basename $path | awk -F- '{print $3}' | cut -d. -f1-3)
