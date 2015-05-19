e9191379bc72b8b029856a6928f6322854701c94
Remon Sinnema <remon.sinnema@emc.com>
2015-05-19 12:05:37 +0200

    Fixed version in Bintray

--- deploy_to_bintray.sh
+++ deploy_to_bintray.sh
@@ -1,7 +1,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   for package in "core" "gradle"; do
     for path in java/$package/build/libs/*; do
-      curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/$(basename $path)\;bt_package=radl-$package\;bt_version=$(basename $path | awk -F- '{print $3}')\;publish=1\;override=1
+      curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/$(basename $path)\;bt_package=radl-$package\;bt_version=$(basename $path | awk -F- '{print $3}' | cut -d. -f1-3)\;publish=1\;override=1
     done
   done
 fi
