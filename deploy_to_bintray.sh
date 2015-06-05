5b9c2c7cbf119e6832ca59e1eda14856c7f6c4f4
Remon Sinnema <remon.sinnema@emc.com>
2015-06-05 15:12:43 +0200

    Show status of deploying to BinTray

--- deploy_to_bintray.sh
+++ deploy_to_bintray.sh
@@ -2,7 +2,13 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL
   for package in "core" "gradle"; do
     for path in java/$package/build/libs/*; do
       version=$(basename $path | awk -F- '{print $3}' | cut -d. -f1-3)
+      echo Uploading radl-$package-$version to BinTray
       curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/radl/radl-$package/$version/$(basename $path)\;bt_package=radl-$package\;bt_version=$version\;publish=1\;override=1
     done
   done
+else
+  echo TRAVIS_BRANCH=$TRAVIS_BRANCH
+  echo TRAVIS_TAG=$TRAVIS_TAG
+  echo TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST
+  echo TRAVIS_SECURE_ENV_VARS=$TRAVIS_SECURE_ENV_VARS
 fi
