b4f5689fc667b37df1d6e9acdabf05732045ce69
Remon Sinnema <remon.sinnema@emc.com>
2015-07-13 12:22:50 +0200

    Better output of all the uploaded artifacts

--- deploy_to_bintray.sh
+++ deploy_to_bintray.sh
@@ -1,8 +1,10 @@
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   for package in "core" "gradle" "maven"; do
+    echo Uploading radl-$package to BinTray
     for path in java/$package/build/libs/*; do
       version=$(basename $path | awk -F- '{print $3}' | cut -d. -f1-3)
-      echo Uploading radl-$package-$version to BinTray
+      file=$(basename $path)
+      echo "  $file"
       curl -T $path -u$USER:$BINTRAY_KEY https://api.bintray.com/content/radl/RADL/radl/radl-$package/$version/$(basename $path)\;bt_package=radl-$package\;bt_version=$version\;publish=1\;override=1
     done
   done
